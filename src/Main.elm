d89ea48c20ccfdcb2d672f3d5d72f0808488dd7a
Paryz <paryzowy@gmail.com>
2019-08-30 17:46:35 +0200

    Uninstall elm.js and add bootstrap

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,12 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, h1, p, strong, text)
+import Html.Attributes exposing (class)
 
 
 view : Html msg
 view =
-    div []
+    div [ class "jumbotron" ]
         [ h1 [] [ text "Welcome to Shameless Gdańsk" ]
         , p [] [ text "Work in progress" ]
         , p [] [ strong [] [ text "Keep in touch" ] ]

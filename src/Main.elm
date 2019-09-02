cb930059f01e50cc922f7eb75005462c35315a4e
Paryz <paryzowy@gmail.com>
2019-09-02 19:43:01 +0200

    working video in chrome

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,26 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, h1, p, source, strong, text, video)
-import Html.Attributes exposing (attribute, autoplay, class, loop, src, type_)
+import Html.Attributes exposing (attribute, autoplay, class, id, loop, src, type_)
 
 
 view : Html msg
 view =
     div [ class "video-wrapper" ]
-        [ video [ class "full-video", autoplay True, loop True, attribute "muted" "" ]
-            [ source [ src "src/assets/public/shameless_gdansk_logo_animation_v2.mp4", type_ "video/mp4" ] [] ]
+        [ video
+            [ class "full-video"
+            , id "video"
+            , attribute "muted" ""
+            , attribute "preload" ""
+            , autoplay True
+            , loop True
+            ]
+            [ source
+                [ src "src/assets/public/shameless_gdansk_logo_animation_v2.mp4"
+                , type_ "video/mp4"
+                ]
+                []
+            ]
         ]
 
 

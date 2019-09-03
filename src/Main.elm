b784f8380d26e30455a7afa11a90e6af8d0aa864
Paryz <paryzowy@gmail.com>
2019-09-03 11:56:30 +0200

    Add fallback image for browser that do not support video

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Bootstrap.Grid as Grid
-import Html exposing (Html, div, h1, p, source, strong, text, video)
+import Html exposing (Html, div, h1, img, p, source, strong, text, video)
 import Html.Attributes exposing (autoplay, class, id, loop, property, src, type_)
 import Json.Encode as Json
 
@@ -21,6 +21,9 @@ view =
                 [ src "src/assets/public/shameless_gdansk_logo_animation_v2.mp4"
                 , type_ "video/mp4"
                 ]
+                []
+            , img
+                [ src "src/assets/public/shameless_gdansk_logo_animation_v2.jpg" ]
                 []
             ]
         ]
